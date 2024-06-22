53a9d56b1ddeba194de3bd62d7176ab043ba3437
sumit <sumitsah250@gmail.com>
2024-06-22 18:08:31 +0545

    bubble sort for vector

--- q20_bubble_sort.cpp
+++ q20_bubble_sort.cpp
@@ -1,8 +1,27 @@
 #include<iostream>
+#include<vector>
 using namespace std;
-void bubblesort(int a[],int N){
+// void bubblesort(int a[],int N){
+//     for(int i=0;i<N;i++){
+//         for(int j=0;j<N-i;j++){
+//             if(a[j]>a[j+1]){
+//                 swap(a[j],a[j+1]);
+//             }
+//         }
+//     }
+// }
+void bubblesort(vector<int> & a,int N){   //not working for size == 4
     for(int i=0;i<N;i++){
         for(int j=0;j<N-i;j++){
+            if(a[j]>a[j+1]){
+                swap(a[j],a[j+1]);
+            }
+        }
+    }
+}
+void bubblesort(vector<int> & a){
+    for(int i=0;i<a.size();i++){
+        for(int j=0;j<a.size()-i;j++){
             if(a[j]>a[j+1]){
                 swap(a[j],a[j+1]);
             }
@@ -12,13 +31,16 @@ void bubblesort(int a[],int N){
 
 int main(){
     int N;
+    int num;
     cout<<"enter the number of elements to be sorted"<<endl;
     cin>>N;
-    int a[N];
+    vector<int> a;
     for(int i=0;i<N;i++){
-        cin>>a[i];
+        cin>>num;
+        a.push_back(num);
     }
-    bubblesort(a,N);
+    // cout<<N<<a.size();
+    bubblesort(a);
     cout<<"sorted array is"<<endl;
     for(int i=0;i<N;i++){
         cout<<a[i]<<endl;
